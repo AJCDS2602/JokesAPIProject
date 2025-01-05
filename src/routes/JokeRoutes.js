e24e7af6ce3d68a213c913a6f9d159ee3698b9d8
AJCDS2602 <abrahamcarranza2614@gmail.com>
2025-01-04 23:24:30 -0400

    Ajustes Para Funcionamiento

--- src/routes/JokeRoutes.js
+++ src/routes/JokeRoutes.js
@@ -1,17 +1,17 @@
 import express from 'express';
-import { getJoke, createJoke, updateJoke, deleteJokeById, getJokeById} from '../controllers/JokeController.js';
+import {deleteJokeById, getJokeById} from '../controllers/JokeController.js';
  
 // Crea un enrutador de Express
 const router = express.Router();
 
-// Define la ruta para obtener un chiste
+/* // Define la ruta para obtener un chiste
 router.get('/getJoke', getJoke);
 
 // Define la ruta para crear un nuevo chiste
 router.post('/createJoke', createJoke);
 
 // Define la ruta para actualizar un chiste existente
-router.put('/updateJoke/:id', updateJoke);
+router.put('/updateJoke/:id', updateJoke); */
 
 // Define la ruta para eliminar un chiste existente por id
 router.delete('/deleteJoke/:id?', deleteJokeById);
